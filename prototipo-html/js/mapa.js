3708471b524dc649ef11667f7d49d6bd81ea6c7d
fabiodettmar <fabio@bluehand.com.br>
2015-04-27 22:18:39 -0300

    teste lendo o json

--- prototipo-html/js/mapa.js
+++ prototipo-html/js/mapa.js
@@ -28,28 +28,28 @@ function abrirInfoBox(id, marker) {
 
 function carregarPontos() {
 	
-	$.getJSON('js/pontos.json', function(pontos) {
+	$.getJSON('js/bus.json', function(pontos) {
 		
 		var latlngbounds = new google.maps.LatLngBounds();
 		
 		$.each(pontos, function(index, ponto) {
 			
 			var marker = new google.maps.Marker({
-				position: new google.maps.LatLng(ponto.Latitude, ponto.Longitude),
-				title: "Meu ponto personalizado! :-D",
+				position: new google.maps.LatLng(ponto.latitude, ponto.longitude),
+				title: "<p>" + ponto.order + "</p>",
 				icon: 'img/marcador.png'
 			});
 			
 			var myOptions = {
-				content: "<p>" + ponto.Descricao + "</p>",
+				content: "<p>" + ponto.sense + "</p>" + "<p>" + ponto.order + "</p>" + "<p>" + ponto.speed + "</p>",
 				pixelOffset: new google.maps.Size(-150, 0)
         	};
 
-			infoBox[ponto.Id] = new InfoBox(myOptions);
-			infoBox[ponto.Id].marker = marker;
+			infoBox[ponto.order] = new InfoBox(myOptions);
+			infoBox[ponto.order].marker = marker;
 			
-			infoBox[ponto.Id].listener = google.maps.event.addListener(marker, 'click', function (e) {
-				abrirInfoBox(ponto.Id, marker);
+			infoBox[ponto.order].listener = google.maps.event.addListener(marker, 'click', function (e) {
+				abrirInfoBox(ponto.order, marker);
 			});
 			
 			markers.push(marker);
